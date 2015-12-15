4648d8fe01c833226b41c72293368217f1b8160b
LoFiRobot <halo@lofirobot.com>
2015-12-15 14:38:27 +0100

    Update lofirobot_extension.js

--- lofirobot_extension.js
+++ lofirobot_extension.js
@@ -119,7 +119,7 @@
     // ping device regularly to check connection
     pinger = setInterval(function() {
       if (pinging) {
-        if (++pingCount > 500) {
+        if (++pingCount > 1000) {
           clearInterval(pinger);
           pinger = null;
           connected = false;
